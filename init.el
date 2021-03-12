4f72ea08eadf121029e55259d532f797a9a82cd3
Ulas Turkmen <ulas.tuerkmen@gmail.com>
2021-03-12 10:35:45 +0100

    Fixed font name

--- init.el
+++ init.el
@@ -116,7 +116,7 @@
 
 ;; Alternatives: InputMono 10, Inconsolata 12, SourceCodePro 10, UbuntuMono 12, FiraCode 12
 ;; IBMPlexMono 10
-(let ((font-name-size "IBMPlexMono 10"))
+(let ((font-name-size "IBM Plex Mono 10"))
   (condition-case nil
       (set-face-attribute 'default nil :font font-name-size)
     (error (message
